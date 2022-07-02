61b91b6afee2c4abce05f141faef0c615cbe62f6
Pedro Parra <pedroparra06091994@gmail.com>
2022-07-02 23:48:51 +0200

    counter match end

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import AlertTraps from "./components/alertTraps.js";
 
 const App = () => {
+  // USESTATE THAT I NEED
   const [board, setBoard] = useState([
     [null, null, null],
     [null, null, null],
     [null, null, null],
   ]);
   const [turn, setTurn] = useState("X");
+  const [count, setCount] = useState({ X: 0, O: 0 });
 
   // Rotate matrix for checkWinner in columns
   const rotateMatrix = () => {
@@ -21,7 +23,25 @@ const App = () => {
   const changeTurn = () => {
     turn === "X" ? setTurn("O") : setTurn("X");
   };
-
+  // Assign values to square and validate the square is empty
+  const assignSquaredValue = (row, column) => {
+    if (board[row][column] === null) {
+      board[row][column] = turn;
+    } else {
+      alert("Ese espacio no esta vacio");
+    }
+  };
+  // Counter of winners to all matchs
+  const matchCounter = () => {
+    alert(`ha ganado ${turn}`);
+    turn === "X" ? (count["X"] += 1) : (count["O"] += 1);
+    setBoard([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ]);
+  };
+  // Check Winner for line in horizontally for matrix and RotateMatrix
   const checkWinnerInLine = (typeBoard) => {
     let Length = typeBoard[0].length;
     for (let arr of typeBoard) {
@@ -30,25 +50,12 @@ const App = () => {
           (arr[i] === "X" && arr[i + 1] === "X" && arr[i + 2] === "X") ||
           (arr[i] === "O" && arr[i + 1] === "O" && arr[i + 2] === "O")
         ) {
-          alert(`Ha ganado ${turn}`);
-          window.location.reload(false);
+          matchCounter();
         }
       }
     }
   };
-  const assignSquaredValue = (row, column) => {
-    if (board[row][column] === null) {
-      board[row][column] = turn;
-      checkWinnerInLine(rotateMatrix());
-      checkWinnerInLine(board);
-      checkWinnerDiagonal(board);
-      checkWinnerDiagonal(rotateMatrix());
-      changeTurn();
-    } else {
-      alert("No te pases de listo");
-    }
-  };
-
+  // Check Winner for diagonal for matrix and RotateMatrix
   const checkWinnerDiagonal = (typeboard) => {
     let Length = typeboard.length;
     var diagonalUno = [];
@@ -60,20 +67,29 @@ const App = () => {
         }
       }
     }
-    console.log(diagonalUno);
     if (
       diagonalUno[0] === diagonalUno[1] &&
       diagonalUno[0] === diagonalUno[2] &&
       diagonalUno[0] !== undefined
     ) {
-      alert(`ha ganado ${turn}`);
-      window.location.reload(false);
+      matchCounter();
     }
+  };
+
+  // Execution of all functions except assignSquareValue. This funct and assign excuse always when i clicked one square
+  const allcheckWinnersAndChangeTurn = (row, column) => {
+    checkWinnerInLine(board);
+    checkWinnerInLine(rotateMatrix());
+    checkWinnerDiagonal(board);
+    checkWinnerDiagonal(rotateMatrix());
+    changeTurn();
   };
 
   return (
     <>
       <AlertTraps />
+      <span className="countx X">X: {count.X}</span>
+      <span className="counto O">{count.O} :O</span>
       <table className="board">
         <tbody>
           {board.map((row, i) => (
@@ -83,8 +99,9 @@ const App = () => {
                   <p
                     onClick={() => {
                       assignSquaredValue(i, j);
+                      allcheckWinnersAndChangeTurn();
                     }}
-                    className="square"
+                    className={`square ${column === "X" ? "X" : "O"}`}
                   >
                     {column}
                   </p>
